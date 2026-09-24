d76a7c87e535d0101ce537ed28506a5eb6583c63
MarilynMonroe11 <324602932+MarilynMonroe11@users.noreply.github.com>
2026-09-24 21:41:07 +0300

    Add text change button

--- app/src/main/java/com/example/madtlab1/MainActivity.java
+++ app/src/main/java/com/example/madtlab1/MainActivity.java
@@ -2,11 +2,26 @@ package com.example.madtlab1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private TextView MainTextView;
+    private Button ChangeTextButton;
+
     @Override
     protected void onCreate(Bundle SavedInstanceState) {
         super.onCreate(SavedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainTextView = findViewById(R.id.main_text_view);
+        ChangeTextButton = findViewById(R.id.change_text_button);
+
+        ConfigureTextButton();
+    }
+
+    private void ConfigureTextButton() {
+        ChangeTextButton.setOnClickListener(View ->
+                MainTextView.setText(R.string.changed_text));
     }
 }
