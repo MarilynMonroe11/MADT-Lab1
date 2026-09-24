5619ac7e6f0d75842aa78f4983baa29fb67652d2
MarilynMonroe11 <324602932+MarilynMonroe11@users.noreply.github.com>
2026-09-24 21:41:07 +0300

    Add text color button

--- app/src/main/java/com/example/madtlab1/MainActivity.java
+++ app/src/main/java/com/example/madtlab1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.madtlab1;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
     private TextView MainTextView;
     private Button ChangeTextButton;
+    private Button ChangeTextColorButton;
 
     @Override
     protected void onCreate(Bundle SavedInstanceState) {
@@ -16,12 +18,19 @@ public class MainActivity extends Activity {
 
         MainTextView = findViewById(R.id.main_text_view);
         ChangeTextButton = findViewById(R.id.change_text_button);
+        ChangeTextColorButton = findViewById(R.id.change_text_color_button);
 
         ConfigureTextButton();
+        ConfigureTextColorButton();
     }
 
     private void ConfigureTextButton() {
         ChangeTextButton.setOnClickListener(View ->
                 MainTextView.setText(R.string.changed_text));
+    }
+
+    private void ConfigureTextColorButton() {
+        ChangeTextColorButton.setOnClickListener(View ->
+                MainTextView.setTextColor(Color.RED));
     }
 }
