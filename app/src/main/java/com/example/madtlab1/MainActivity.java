746e723030c540c9510416668ffb2a9f19795390
MarilynMonroe11 <324602932+MarilynMonroe11@users.noreply.github.com>
2026-09-24 21:41:08 +0300

    Add code marker for revert

--- app/src/main/java/com/example/madtlab1/MainActivity.java
+++ app/src/main/java/com/example/madtlab1/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    //Code for revert
     private View RootLayout;
     private TextView MainTextView;
     private Button ChangeTextButton;
